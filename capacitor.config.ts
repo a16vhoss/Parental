45edf6f1c73cd3fad49dd470a5b431aab7243c3c
Andree villegas hossfeldt <a_villehoss@MacBook-Air-de-Andree.local>
2026-01-19 21:12:46 -0600

    feat: add Capacitor iOS setup for App Store

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.thebabycareboxapp',
+  appName: 'The Baby Care Box',
+  webDir: 'dist'
+};
+
+export default config;
